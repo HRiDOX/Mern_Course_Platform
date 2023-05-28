c64bb160dfc513d8690c70197ef7c70c3421c629
HRiDOX <rgrrf123@gmail.com>
2023-05-28 20:28:56 +0600

    home page setup

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import vg from "../../assets/images/bg.png"
 import {CgGoogle,CgYoutube} from "react-icons/cg"
 import {SiCoursera,SiUdemy} from  "react-icons/si";
 import {DiAws} from "react-icons/di"
+import introVideo from "../../assets/videos/intro.mp4"
 const Home = () => {
   return (
    <section className='home'>
@@ -38,15 +39,25 @@ const Home = () => {
     </div>
     <Box padding={"8"} bg="blackAlpha.800">
         <Heading textAlign={"center"} fontFamily="body" color={"yellow.400"} children="Brands"/>
-    </Box>
-    <HStack className='brandsBanner'>
+    
+    <HStack className="brandsBanner" justifyContent={"space-evenly"} marginTop="4">
         <CgGoogle/>
         <CgYoutube/>
         <SiCoursera/>
         <SiUdemy/>
         <DiAws/>
-    </HStack>
-    
+    </HStack>f
+    </Box>
+    <div className="container2">
+        <video 
+        autoPlay 
+        controls 
+        controlsList="nodownload nofullscreen noremoteplayback"
+        disablePictureInPicture
+        disableRemotePlayback
+        src={introVideo}></video>
+
+    </div>
 
    </section>
   )
